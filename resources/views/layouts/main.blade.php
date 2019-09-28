95210b5e011919e9f1df355d1674731e33af187c
Kevin Zheng <kzheng@email.sc.edu>
2019-09-28 16:39:10 -0400

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -65,6 +65,26 @@
 						<a class="dropdown-item" href="/usertypes">Types</a>
 					</div>
 				</li>
+                <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fas fa-fw fa-users"></i>
+						<span>Location</span>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="/users">Users</a>
+						<a class="dropdown-item" href="/usertypes">Types</a>
+					</div>
+				</li>
+                <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fas fa-fw fa-users"></i>
+						<span>Orders</span>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="/users">Users</a>
+						<a class="dropdown-item" href="/usertypes">Types</a>
+					</div>
+				</li>
 			</ul>
 			<div id="wrapper">
 				<div class="container-fluid">
